ec5fdd3604713802571afa94a6fd63e388992a54
Dokyme <zdkscope@qq.com>
2018-05-15 15:33:33 +0800

    Aha

---
+++ src/main/java/com/dokyme/alg4/sorting/priorityqueue/IndexMinPQ.java
@@ -0,0 +1,15 @@
+package com.dokyme.alg4.sorting.priorityqueue;
+
+/**
+ * Created by intellij IDEA.But customed by hand of Dokyme.
+ *
+ * @author dokym
+ * @date 2018/5/11-10:22
+ * Description:
+ */
+public class IndexMinPQ {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/dokyme/alg4/sorting/priorityqueue/LinkedMaxHeap.java
@@ -0,0 +1,15 @@
+package com.dokyme.alg4.sorting.priorityqueue;
+
+/**
+ * Created by intellij IDEA.But customed by hand of Dokyme.
+ *
+ * @author dokym
+ * @date 2018/5/12-20:13
+ * Description:
+ */
+public class LinkedMaxHeap {
+
+    public static void main(String[] args) {
+
+    }
+}
